d3dff55dcee16411e01c9116374394a14deb3583
pxp9 <pepe.marquezromero@gmail.com>
2021-12-26 19:27:10 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,9 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
-
-fn main() {
-    
-    let secret_number = rand::thread_rng().gen_range(1..101); 
-    
-    println!("Guess the number!\n");
-
-    let mut found = false;
-    
-    while(!found){
-
-    println!("Please input your guess.\n");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
+fn compare_guess(guess : u32 , secret_number:u32) ->  bool {
     println!("You guessed: {}", guess);
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
+    let mut found = false;
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
@@ -33,6 +13,36 @@ fn main() {
             found = true;
         }
     }
-    }
-    
+    found
 }
+
+fn read_number() -> String{
+    println!("Please input a number to guess.\n");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line\n");
+    guess
+}
+fn main() {
+
+    let secret_number:u32 = rand::thread_rng().gen_range(1..101); 
+
+    println!("Guess the number!\n");
+
+    let mut found = false;
+    let mut guess:String = read_number();
+
+    while !found {
+        match guess.trim().parse::<u32>() {
+            Ok(guess_number) =>{
+                found = compare_guess(guess_number , secret_number);
+                if !found {
+                    guess = read_number();
+                }
+            } 
+            Err(_e) => guess = read_number(),
+        }
+    }
+}
+
